5d165d8013b812d87a02918f5e9be821af7c42f4
Meezu Lawot <meezu.lawot@swifttech.com.np>
2023-10-17 10:44:47 +0545

    Initial Commit

---
+++ MoyaProtobufIntegration/MoyaProtobufIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoyaProtobufIntegrationApp.swift
+//  MoyaProtobufIntegration
+//
+//  Created by Meezu Lawot on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoyaProtobufIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
